aa934e2a0ec70e54cd8e7d88f7aaa0fa651bc584
Elvis Martins <elvisbmartins@gmail.com>
2018-02-27 16:56:27 -0300

    remove duplicate code

--- metabox_example.php
+++ metabox_example.php
@@ -30,7 +30,6 @@ class add_metabox_name{
             $content = $metadata['metabox_name'][0]; //Name do Metabox
         };
     ?>  
-
         <!-- Tipos de Campos-->
         <textarea name="<?php echo __CLASS__ ?>[metabox_name]" style="background-color: #fff; font-size: 1.7em; height: 4em; line-height: 100%; margin: 0; outline: 0; padding: 3px 8px; width: 100%;">
         	<?php echo $content; ?>
@@ -42,45 +41,11 @@ class add_metabox_name{
           <!-- Tipos de Campos-->
         <input type="text" name="<?php echo __CLASS__ ?>[metabox_name]" value="<?php echo  $content; ?>" style="background-color: #fff; font-size: 1.7em; height: 1.7em; line-height: 100%; margin: 0; outline: 0; padding: 3px 8px; width: 100%;" />
 
-
-
          <!-- Tipos de Campos -  Campo para adicionar icones, adicionar js de fontawesome-->
         <input data-hide-on-select="true" name="<?php echo __CLASS__ ?>[icone_awesome]" class="js-icon-rede-social form-control pck pck-auto picker-element picker-input " value="<?php echo $icone; ?>" type="text">
         <span class="input-group-addon iconpicker-component"><i class="fa fa-fw fa-archive"></i></span>
 
         <?php
-    }
-    static function savePost($post_id) {
-        // verify if this is an auto save routine.
-        // If it is our form has not been submitted, so we dont want to do anything
-        if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE)
-            return;
-        // verify this came from the our screen and with proper authorization,
-        // because save_post can be triggered at other times
-        if(!isset($_POST[__CLASS__.'_noncename']))
-            return;
-        if (!wp_verify_nonce($_POST[__CLASS__.'_noncename'], 'save_'.__CLASS__))
-            return;
-        // Check permissions
-        if ('page' == $_POST['post_type']) {
-            if (!current_user_can('edit_page', $post_id))
-                return;
-        }
-        else {
-            if (!current_user_can('edit_post', $post_id))
-                return;
-        }
-        // OK, we're authenticated: we need to find and save the data
-        if(isset($_POST[__CLASS__])){
-            foreach($_POST[__CLASS__] as $meta_key => $value)
-                update_post_meta($post_id, $meta_key, $value);
-        }
-    }
-}
-add_metabox_name::init();
-
-    
-    <?php
     }
     static function savePost($post_id) {
         // verify if this is an auto save routine.
